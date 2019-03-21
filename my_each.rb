7ea8eb93b57fb33bb5a7e4f2df83731e2265c66d
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-21 10:42:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,10 @@
-def my_each(collection)
- 
- i=0
- while i < collection.length 
- yield(collection[i])
- i+=1
- end 
- collection
+def my_each(array) # put argument(s) here
+  i = 0
+  while i < array.size
+    yield(array[i]) # if block, then yield to block # also need to pass argument(s) to yield, otherwise block is operating on nil
+    i += 1
+  end
+array
 end
 
 my_each(collection) {|x| puts x}
